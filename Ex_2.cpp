12f06a01414449a38eab4cf508f517a979099ed6
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 12:03:56 -0300

    Ex_2.cpp

--- Ex_2.cpp
+++ Ex_2.cpp
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main() {
-    // Declaração das variáveis
-    int num1, num2;
 
-    // Solicita ao usuário que insira os dois números
-    printf("Digite o primeiro número (inteiro): ");
-    scanf("%d", &num1);
+// DeclaraÃ§Ã£o das variÃ¡veis
+int num1, num2;
 
-    printf("Digite o segundo número (inteiro): ");
-    scanf("%d", &num2);
+// Solicita que o usuÃ¡rio preencha os dois nÃºmeros
+printf("Digite o primeiro nÃºmero (inteiro): ");
+scanf("%d", &num1);
 
-    // Usando incremento para calcular a soma
-    for (int i = 0; i < num2; ++i) {
-        ++num1; // Incrementa num1 pelo valor de num2
-    }
+printf("Digite o segundo nÃºmero (inteiro): ");
+scanf("%d", &num2);
 
-    // Exibe o resultado
-    printf("A soma é: %d\n", num1);
-    
-    getchar();
-    return 0;
+// Adicionar num2 a num1 sem usar o operador adicional
+for (int i = 0; i < num2; ++i)
+{
+    ++num1;
 }
 
+// Exibe o resultado
+printf("A soma Ã©: %d\n", num1);
+
+getchar();
+return 0;
+}
