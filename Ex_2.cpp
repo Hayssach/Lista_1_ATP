491bbd03bbda0d281db73a701bcfe0017abef1e5
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 16:05:40 -0300

    Ex_2.cpp

--- Ex_2.cpp
+++ Ex_2.cpp
@@ -4,23 +4,23 @@
 int main() {
 
 // Declaração das variáveis
-int num1, num2;
+int numero1, numero2;
 
 // Solicita que o usuário preencha os dois números
 printf("Digite o primeiro número (inteiro): ");
-scanf("%d", &num1);
+scanf("%d", &numero1);
 
 printf("Digite o segundo número (inteiro): ");
-scanf("%d", &num2);
+scanf("%d", &numero2);
 
 // Adicionar num2 a num1 sem usar o operador adicional
-for (int i = 0; i < num2; ++i)
+for (int i = 0; i < numero2; ++i)
 {
-    ++num1;
+    ++numero1;
 }
 
 // Exibe o resultado
-printf("A soma é: %d\n", num1);
+printf("A soma é: %d\n", numero1);
 
 getchar();
 return 0;
